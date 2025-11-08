c7f44009d44308b6f2aa59da2f06aca24baf5a56
leesinma1214 <revolver1214@gmail.com>
2025-11-08 05:06:04 +0000

    Add react-icons dependency and implement Navbar component with icons

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -1,9 +1,38 @@
 import { type FC } from "react";
+import {
+  Navbar as HeroUINav,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Badge,
+} from "@heroui/react";
+import { FaBookReader } from "react-icons/fa";
+import { FaCartShopping } from "react-icons/fa6";
+import { Link } from "react-router-dom";
 
 interface Props {}
 
 const Navbar: FC<Props> = () => {
-  return <div className=""></div>;
+  return (
+    <HeroUINav>
+      <NavbarBrand>
+        <Link to="/" className="flex items-center justify-center space-x-2">
+          <FaBookReader size={24} />
+          <p className="font-bold text-inherit">DIGIBook</p>
+        </Link>
+      </NavbarBrand>
+
+      <NavbarContent justify="end">
+        <NavbarItem>
+          <Link to="/cart">
+            <Badge content="0" color="danger" shape="circle">
+              <FaCartShopping size={24} />
+            </Badge>
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+    </HeroUINav>
+  );
 };
 
 export default Navbar;

4ff4dbbd06f13858dd55ac5cb92c8495ce6aaf92
leesinma1214 <revolver1214@gmail.com>
2025-11-11 04:11:09 +0000

    Setup Redux toolkit and integrate with main application

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { HeroUIProvider } from '@heroui/react'
-import { BrowserRouter } from 'react-router-dom'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { HeroUIProvider } from "@heroui/react";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <HeroUIProvider>
-        <App />
-      </HeroUIProvider>
+      <Provider store={store}>
+        <HeroUIProvider>
+          <App />
+        </HeroUIProvider>
+      </Provider>
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);

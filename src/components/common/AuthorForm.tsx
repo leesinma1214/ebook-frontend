15925ead10ff7a7f626cde04a18deed2fe545180
leesinma1214 <revolver1214@gmail.com>
2025-11-17 04:40:15 +0000

    feat: implement social links management in AuthorForm and update NewAuthorRegistration to pass button title

--- src/components/common/AuthorForm.tsx
+++ src/components/common/AuthorForm.tsx
@@ -1,7 +1,65 @@
-import { type FC } from "react";
+import { type FC, useState } from "react";
+import useAuth from "../../hooks/useAuth";
+import RichEditor from "../rich-editor";
+import { Button, Input } from "@heroui/react";
+import { MdClose, MdOutlineAdd } from "react-icons/md";
 
-const AuthorForm: FC = () => {
-  return <div></div>;
+interface Props {
+  btnTitle?: string;
+}
+
+const AuthorForm: FC<Props> = ({ btnTitle }) => {
+  const [socialLinks, setSocialLinks] = useState([""]);
+  const { profile } = useAuth();
+
+  const addLinkFields = () => {
+    setSocialLinks([...socialLinks, ""]);
+  };
+
+  const removeLinkField = (index: number) => {
+    const oldList = [...socialLinks];
+    oldList.splice(index, 1);
+
+    setSocialLinks(oldList);
+  };
+
+  return (
+    <div className="p-4 space-y-6">
+      <p>
+        Name: <span className="font-semibold text-lg">{profile?.name}</span>
+      </p>
+
+      <RichEditor editable placeholder="Hãy nói cho độc giả biết bạn là ai..." />
+
+      <div className="space-y-4">
+        <p className="text-sm font-semibold">Mạng xã hội</p>
+        {socialLinks.map((_, index) => {
+          return (
+            <div className="flex items-center space-x-4">
+              <Input placeholder="https://x.com/@something" key={index} />
+              {socialLinks.length > 1 && (
+                <Button
+                  size="sm"
+                  onPress={() => removeLinkField(index)}
+                  isIconOnly
+                >
+                  <MdClose />
+                </Button>
+              )}
+            </div>
+          );
+        })}
+
+        <div className="flex justify-end">
+          <Button size="sm" onPress={addLinkFields} isIconOnly>
+            <MdOutlineAdd />
+          </Button>
+        </div>
+      </div>
+
+      <Button>{btnTitle}</Button>
+    </div>
+  );
 };
 
 export default AuthorForm;
--- src/views/NewAuthorRegistration.tsx
+++ src/views/NewAuthorRegistration.tsx
@@ -2,7 +2,7 @@ import { type FC } from "react";
 import AuthorForm from "../components/common/AuthorForm";
 
 const NewAuthorRegistration: FC = () => {
-  return <AuthorForm />;
+  return <AuthorForm btnTitle="Trở thành Tác Giả"/>;
 };
 
 export default NewAuthorRegistration;

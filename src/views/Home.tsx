add2ff492079e8182de4fc2bb9936c83b368d60a
leesinma1214 <revolver1214@gmail.com>
2025-11-21 03:58:03 +0000

    Add BookByGenre to fetching book by genre

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,11 +1,13 @@
 import { type FC } from "react";
 import HeroSection from "../components/HeroSection";
+import BookByGenre from "../components/BookByGenre";
 
 const Home: FC = () => {
 
   return (
     <div>
       <HeroSection />
+      <BookByGenre genre="Hiện Thực" />
     </div>
   );
 };

de5946a051224339536f57a4ffe9a0c1379a092a
leesinma1214 <revolver1214@gmail.com>
2025-11-22 10:58:47 +0000

    feat: enhance Cart functionality by adding loading skeleton and integrating clearCart feature; update CartProvider to manage fetching state and total calculations

--- src/views/Cart.tsx
+++ src/views/Cart.tsx
@@ -1,9 +1,155 @@
 import { type FC } from "react";
 import useCart from "../hooks/useCart";
+import Skeletons from "../components/skeletons";
+import { Button, Chip, Divider } from "@heroui/react";
+import { calculateDiscount, formatPrice } from "../utils/helper";
+import { FaMinus, FaPlus, FaRegTrashCan } from "react-icons/fa6";
+import { MdOutlineShoppingCartCheckout } from "react-icons/md";
 
 const Cart: FC = () => {
-  const { items } = useCart();
-  return <div>{JSON.stringify(items)}</div>;
+  const {
+    pending,
+    items,
+    totalCount,
+    fetching,
+    subTotal,
+    totalPrice,
+    updateCart,
+    clearCart,
+  } = useCart();
+
+  if (fetching) return <Skeletons.Cart />;
+
+  if (!totalCount)
+    return (
+      <div className="lg:p-0 p-5">
+        <h1 className="text-xl mb-6 font-semibold">Giỏ hàng</h1>
+        <div className="p-5 text-center">
+          <h1 className="font-semibold text-3xl opacity-40">
+            Giỏ hàng trống!
+          </h1>
+        </div>
+      </div>
+    );
+
+  return (
+    <div className="lg:p-0 p-5">
+      <div className="flex justify-between items-center">
+        <h1 className="text-xl mb-6 font-semibold">Giỏ hàng</h1>
+        <button onClick={clearCart} className="underline">
+          Làm trống giỏ hàng
+        </button>
+      </div>
+      <div className="space-y-6">
+        {items.map(({ product, quantity }) => {
+          return (
+            <div key={product.id} className="flex">
+              {/* Product Image */}
+
+              <img
+                src={product.cover}
+                alt={product.title}
+                className="w-28 h-[185px] object-cover rounded"
+              />
+
+              {/* Product Details */}
+              <div className="md:grid flex flex-col grid-cols-6">
+                <div className="p-5 col-span-5">
+                  <h1>{product.title}</h1>
+
+                  <div className="flex space-x-2">
+                    <Chip color="danger">
+                      Giảm {calculateDiscount(product.price)}%
+                    </Chip>
+                    <h1 className="line-through italic">
+                      {formatPrice(Number(product.price.mrp))}
+                    </h1>
+                  </div>
+
+                  <div className="flex space-x-2">
+                    <h1 className="font-bold">
+                      {formatPrice(Number(product.price.sale))}
+                    </h1>
+
+                    <span>x {quantity}</span>
+                  </div>
+                </div>
+
+                {/* Cart Control */}
+                <div className="col-span-1 flex items-center space-x-3 p-5 md:p-0">
+                  <Button
+                    isIconOnly
+                    variant="solid"
+                    size="sm"
+                    isLoading={pending}
+                    onPress={() => updateCart({ product, quantity: -1 })}
+                  >
+                    <FaMinus />
+                  </Button>
+                  <Chip radius="sm" variant="bordered">
+                    {quantity}
+                  </Chip>
+                  <Button
+                    isIconOnly
+                    variant="solid"
+                    size="sm"
+                    isLoading={pending}
+                    onPress={() => updateCart({ product, quantity: 1 })}
+                  >
+                    <FaPlus />
+                  </Button>
+                  <Button
+                    isIconOnly
+                    variant="solid"
+                    size="sm"
+                    isLoading={pending}
+                    onPress={() => updateCart({ product, quantity: -quantity })}
+                  >
+                    <FaRegTrashCan />
+                  </Button>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+
+      <Divider className="my-6" />
+
+      <div className="md:block flex justify-between items-end">
+        <div className="text-right space-y-1">
+          <h1 className="font-semibold text-xl">Tổng giỏ hàng</h1>
+          <Divider />
+          <p className="line-through italic">{formatPrice(subTotal)}</p>
+          <p className="font-semibold text-xl">{formatPrice(totalPrice)}</p>
+        </div>
+
+        <div className="text-right md:mt-3">
+          <Button
+            color="danger"
+            radius="sm"
+            size="lg"
+            isLoading={pending}
+            startContent={<MdOutlineShoppingCartCheckout size={18} />}
+          >
+            Thanh toán
+          </Button>
+          <div className="mt-3">
+            <Chip size="sm">
+              <p>
+                Bạn đã tiết kiệm tổng cộng{" "}
+                {calculateDiscount({
+                  mrp: subTotal.toFixed(2),
+                  sale: totalPrice.toFixed(2),
+                })}
+                %
+              </p>
+            </Chip>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Cart;

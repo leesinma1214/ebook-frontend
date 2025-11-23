625f7170d8590515fdf782884bb3f42bbcda4e36
leesinma1214 <revolver1214@gmail.com>
2025-11-23 14:44:56 +0000

    feat: enhance ReviewForm component with rating selection and styling

--- src/views/ReviewForm.tsx
+++ src/views/ReviewForm.tsx
@@ -1,16 +1,46 @@
-import { type FC } from "react";
+import { Button } from "@heroui/react";
+import { type FC, useState } from "react";
+import { FaRegStar, FaStar } from "react-icons/fa6";
 import { useParams } from "react-router-dom";
+import RichEditor from "../components/rich-editor";
 
-const ReviewForm: FC = () => {
+interface Props {}
+
+const ReviewForm: FC<Props> = () => {
+  const [selectedRatings, setSelectedRatings] = useState<string[]>([]);
   const { bookId } = useParams();
 
+  const updateRatingChanges = (rating: number) => {
+    const newRatings = Array<string>(rating).fill("selected");
+    setSelectedRatings(newRatings);
+  };
+
+  const ratings = Array(5).fill("");
+
   return (
-    <form>
-      {/* Rating Bar */}
+    <form className="p-5 space-y-6">
+      {ratings.map((_, index) => {
+        return (
+          <Button
+            isIconOnly
+            color="warning"
+            variant="light"
+            radius="full"
+            key={index}
+            onPress={() => updateRatingChanges(index + 1)}
+          >
+            {selectedRatings[index] === "selected" ? (
+              <FaStar size={24} />
+            ) : (
+              <FaRegStar size={24} />
+            )}
+          </Button>
+        );
+      })}
 
-      {/* Editor (Rich Editor) */}
+      <RichEditor placeholder="Viết về cuốn sách..." />
 
-      {/* Submit Button */}
+      <Button type="submit">Đánh giá</Button>
     </form>
   );
 };

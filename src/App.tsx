c33228219293e7dcb672a11a209bfa6443baf44f
leesinma1214 <revolver1214@gmail.com>
2025-11-23 03:56:24 +0000

    feat: add PaymentSuccess component and integrate routing in App for successful payments

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,7 @@ import Author from "./routes/Author";
 import NotFound from "./views/NotFound";
 import SingleBook from "./views/SingleBook";
 import Cart from "./views/Cart";
+import PaymentSuccess from "./views/PaymentSuccess";
 
 const App: FC = () => {
   return (
@@ -31,6 +32,7 @@ const App: FC = () => {
 
         <Route element={<Private />}>
           <Route path="/profile" element={<Profile />} />
+          <Route path="/payment-success" element={<PaymentSuccess />} />
           <Route path="/update-profile" element={<UpdateProfile />} />
           <Route path="/new-user" element={<NewUser />} />
           <Route

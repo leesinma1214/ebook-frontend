e8ffe2bf6210f795afffe9c6d167feee48e1d6af
leesinma1214 <revolver1214@gmail.com>
2025-11-24 09:52:41 +0000

    feat: add EpubReader component and integrate with ReadingPage
    - Added EpubReader component to render EPUB files.
    - Updated ReadingPage to fetch book URL from API and pass it to EpubReader.
    - Included epubjs library in package.json for EPUB handling.

--- src/views/ReadingPage.tsx
+++ src/views/ReadingPage.tsx
@@ -1,7 +1,40 @@
-import { type FC } from "react";
+import { type FC, useEffect, useState } from "react";
+import EpubReader from "../components/EpubReader";
+import client from "../api/client";
+import { useParams } from "react-router-dom";
+import { parseError } from "../utils/helper";
+
+interface BookAPIRes {
+  settings: {
+    highlights: string[];
+    lastLocation: string;
+  };
+  url: string;
+}
 
 const ReadingPage: FC = () => {
-  return <div></div>;
+  const [url, setUrl] = useState("");
+  const { slug } = useParams();
+  useEffect(() => {
+    if (!slug) return;
+
+    const fetchBookUrl = async () => {
+      try {
+        const { data } = await client.get<BookAPIRes>(`/book/read/${slug}`);
+        setUrl(data.url);
+      } catch (error) {
+        parseError(error);
+      }
+    };
+
+    fetchBookUrl();
+  }, []);
+
+  return (
+    <div>
+      <EpubReader url={url} />
+    </div>
+  );
 };
 
 export default ReadingPage;

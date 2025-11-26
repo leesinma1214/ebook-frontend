b50592bc20080945c1400dc401a28e15ade95902
leesinma1214 <revolver1214@gmail.com>
2025-11-26 04:30:19 +0000

    feat: enhance EpubReader to support highlight management and integrate with ReadingPage

--- src/components/reader/index.tsx
+++ src/components/reader/index.tsx
@@ -14,7 +14,14 @@ import HighlightOptions from "./HighlightOptions";
 interface Props {
   url: string;
   title?: string;
+  highlights: Highlight[];
+  onHighlight(data: Highlight): void;
 }
+
+export type Highlight = {
+  selection: string;
+  fill: string;
+};
 
 const container = "epub_container";
 const wrapper = "epub_wrapper";
@@ -114,7 +121,24 @@ const loadTableOfContent = async (book: Book) => {
   return navLabels;
 };
 
-const EpubReader: FC<Props> = ({ url, title }) => {
+const applyHighlights = async (
+  rendition: Rendition,
+  highlights: Highlight[]
+) => {
+  highlights.forEach(({ selection, fill }) => {
+    rendition.annotations.highlight(
+      selection,
+      undefined,
+      undefined,
+      undefined,
+      {
+        fill,
+      }
+    );
+  });
+};
+
+const EpubReader: FC<Props> = ({ url, title, highlights, onHighlight }) => {
   const [loading, setLoading] = useState(true);
   const [showHighlightOption, setShowHighlightOptions] = useState(false);
   const [selectedCfi, setSelectedCfi] = useState("");
@@ -124,7 +148,6 @@ const EpubReader: FC<Props> = ({ url, title }) => {
   const [settings, setSettings] = useState({
     fontSize: 22,
   });
-
   const [page, setPage] = useState({
     start: 0,
     end: 0,
@@ -137,6 +160,15 @@ const EpubReader: FC<Props> = ({ url, title }) => {
     const end = location.end.displayed.page;
     const total = location.start.displayed.total;
     setPage({ start, end, total });
+  };
+
+  const handleHighlightSelection = (fill: string) => {
+    if (!rendition) return;
+
+    const newHighlight = { fill, selection: selectedCfi };
+    applyHighlights(rendition, [newHighlight]);
+    setShowHighlightOptions(false);
+    onHighlight(newHighlight);
   };
 
   const handleNavigation = (href: string) => {
@@ -175,7 +207,11 @@ const EpubReader: FC<Props> = ({ url, title }) => {
     if (!rendition) return;
     // basic book styling
     rendition.themes.fontSize(settings.fontSize + "px");
-  }, [rendition]);
+
+    rendition.on("displayed", () => {
+      applyHighlights(rendition, highlights);
+    });
+  }, [rendition, highlights]);
 
   useEffect(() => {
     if (!url) return;
@@ -200,11 +236,14 @@ const EpubReader: FC<Props> = ({ url, title }) => {
 
     // Let's listen to the text selection
     rendition.on("selected", (cfi: string) => {
+      console.log(cfi);
       setShowHighlightOptions(true);
       setSelectedCfi(cfi);
     });
 
-    rendition.on("displayed", () => updatePageCounts(rendition));
+    rendition.on("displayed", () => {
+      updatePageCounts(rendition);
+    });
     rendition.on("locationChanged", () => updatePageCounts(rendition));
 
     loadTableOfContent(book)
@@ -223,6 +262,7 @@ const EpubReader: FC<Props> = ({ url, title }) => {
   return (
     <div className="h-screen flex flex-col group dark:bg-book-dark dark:text-book-dark">
       <LoadingIndicator visible={loading} />
+
       <div className="flex items-center h-14 shadow-md opacity-0 group-hover:opacity-100 transition">
         <div className="max-w-3xl md:mx-auto md:pl-0 pl-5">
           <h1 className="line-clamp-1 font-semibold text-large">{title}</h1>
@@ -276,7 +316,10 @@ const EpubReader: FC<Props> = ({ url, title }) => {
         onClick={handleNavigation}
       />
 
-      <HighlightOptions visible={showHighlightOption} onClear={() => setShowHighlightOptions(false)} onSelect={(color) => {}} />
+      <HighlightOptions
+        visible={showHighlightOption}
+        onSelect={handleHighlightSelection}
+      />
 
       <div className="h-10 flex items-center justify-center opacity-0 group-hover:opacity-100">
         <div className="flex-1 text-center">
--- src/views/ReadingPage.tsx
+++ src/views/ReadingPage.tsx
@@ -1,8 +1,10 @@
 import { type FC, useEffect, useState } from "react";
-import EpubReader from "../components/reader";
+import EpubReader, { type Highlight } from "../components/reader";
 import client from "../api/client";
 import { useParams, useSearchParams } from "react-router-dom";
 import { parseError } from "../utils/helper";
+
+interface Props {}
 
 interface BookAPIRes {
   settings: {
@@ -12,12 +14,20 @@ interface BookAPIRes {
   url: string;
 }
 
-const ReadingPage: FC = () => {
+const ReadingPage: FC<Props> = () => {
   const [url, setUrl] = useState("");
+  const [highlights, setHighlights] = useState<Highlight[]>([
+    { fill: "red", selection: "epubcfi(/6/6!/4/26,/1:250,/1:439)" },
+    { fill: "blue", selection: "epubcfi(/6/6!/4/18,/1:1,/1:193)" },
+  ]);
   const { slug } = useParams();
   const [searchParam] = useSearchParams();
   const title = searchParam.get("title");
-  
+
+  const handleOnHighlightSelection = (data: Highlight) => {
+    setHighlights([...highlights, data]);
+  };
+
   useEffect(() => {
     if (!slug) return;
 
@@ -35,7 +45,12 @@ const ReadingPage: FC = () => {
 
   return (
     <div>
-      <EpubReader url={url} title={title || ""} />
+      <EpubReader
+        url={url}
+        title={title || ""}
+        highlights={highlights}
+        onHighlight={handleOnHighlightSelection}
+      />
     </div>
   );
 };

c5e0d4f5ea7041937c9f66ffedc799cf8d50cf61
leesinma1214 <revolver1214@gmail.com>
2025-12-04 11:07:11 +0000

    feat: update Navbar component to include search input and menu icon

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const client = axios.create({
-  // baseURL: "http://192.168.98.2:8989",
-  baseURL: "https://ebook-platform.onrender.com",
+  baseURL: "http://192.168.98.2:8989",
+  // baseURL: "https://ebook-platform.onrender.com",
 });
 
 client.interceptors.request.use(function (config) {
--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -5,6 +5,7 @@ import {
   NavbarContent,
   NavbarItem,
   Badge,
+  Input,
 } from "@heroui/react";
 import { FaBookReader } from "react-icons/fa";
 import { FaCartShopping } from "react-icons/fa6";
@@ -12,32 +13,51 @@ import { Link } from "react-router-dom";
 import ProfileOptions from "../profile/ProfileOptions";
 import DarkModeSwitch from "./DarkModeSwitch";
 import useCart from "../../hooks/useCart";
-  
+import { IoMdSearch } from "react-icons/io";
+import { IoMenu } from "react-icons/io5";
+
 const Navbar: FC = () => {
   const { totalCount } = useCart();
 
   return (
     <HeroUINav>
-      <NavbarBrand>
+      <NavbarBrand className="md:flex hidden">
         <Link to="/" className="flex items-center justify-center space-x-2">
           <FaBookReader size={24} />
           <p className="font-bold text-inherit">DiGiRead</p>
         </Link>
       </NavbarBrand>
 
+      <NavbarContent className="w-full" justify="center">
+        <Input
+          variant="bordered"
+          placeholder="Tìm kiếm sách..."
+          endContent={
+            <button className="focus:outline-none" type="button">
+              <IoMdSearch size={24} />
+            </button>
+          }
+          className="w-full"
+        />
+      </NavbarContent>
+
       <NavbarContent justify="end">
-        <NavbarItem>
+        <NavbarItem className="md:flex hidden">
           <DarkModeSwitch />
         </NavbarItem>
-        <NavbarItem>
+        <NavbarItem className="md:flex hidden">
           <Link to="/cart">
             <Badge content={totalCount} color="danger" shape="circle">
               <FaCartShopping size={24} />
             </Badge>
           </Link>
         </NavbarItem>
-        <NavbarItem>
+        <NavbarItem className="md:flex hidden">
           <ProfileOptions />
+        </NavbarItem>
+
+        <NavbarItem className="flex md:hidden">
+          <IoMenu size={26} />
         </NavbarItem>
       </NavbarContent>
     </HeroUINav>

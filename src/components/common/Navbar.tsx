5b7d7faf924456fe716e308ef09826fb887e1117
leesinma1214 <revolver1214@gmail.com>
2025-12-04 11:26:29 +0000

    feat: implement MobileNav component for responsive navigation

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -1,4 +1,4 @@
-import { type FC } from "react";
+import { type FC, useState } from "react";
 import {
   Navbar as HeroUINav,
   NavbarBrand,
@@ -15,52 +15,69 @@ import DarkModeSwitch from "./DarkModeSwitch";
 import useCart from "../../hooks/useCart";
 import { IoMdSearch } from "react-icons/io";
 import { IoMenu } from "react-icons/io5";
+import MobileNav from "../MobileNav";
 
 const Navbar: FC = () => {
+  const [showNav, setShowNav] = useState(false);
   const { totalCount } = useCart();
 
+  const openNav = () => {
+    setShowNav(true);
+  };
+
+  const closeNav = () => {
+    setShowNav(false);
+  };
+
   return (
-    <HeroUINav>
-      <NavbarBrand className="md:flex hidden">
-        <Link to="/" className="flex items-center justify-center space-x-2">
-          <FaBookReader size={24} />
-          <p className="font-bold text-inherit">DiGiRead</p>
-        </Link>
-      </NavbarBrand>
-
-      <NavbarContent className="w-full" justify="center">
-        <Input
-          variant="bordered"
-          placeholder="Tìm kiếm sách..."
-          endContent={
-            <button className="focus:outline-none" type="button">
-              <IoMdSearch size={24} />
-            </button>
-          }
-          className="w-full"
-        />
-      </NavbarContent>
-
-      <NavbarContent justify="end">
-        <NavbarItem className="md:flex hidden">
-          <DarkModeSwitch />
-        </NavbarItem>
-        <NavbarItem className="md:flex hidden">
-          <Link to="/cart">
-            <Badge content={totalCount} color="danger" shape="circle">
-              <FaCartShopping size={24} />
-            </Badge>
+    <>
+      <HeroUINav>
+        <NavbarBrand className="md:flex hidden">
+          <Link to="/" className="flex items-center justify-center space-x-2">
+            <FaBookReader size={24} />
+            <p className="font-bold text-inherit">DiGiRead</p>
           </Link>
-        </NavbarItem>
-        <NavbarItem className="md:flex hidden">
-          <ProfileOptions />
-        </NavbarItem>
+        </NavbarBrand>
 
-        <NavbarItem className="flex md:hidden">
-          <IoMenu size={26} />
-        </NavbarItem>
-      </NavbarContent>
-    </HeroUINav>
+        <NavbarContent className="w-full" justify="center">
+          <Input
+            variant="bordered"
+            placeholder="Tìm kiếm sách..."
+            endContent={
+              <button className="focus:outline-none" type="button">
+                <IoMdSearch size={24} />
+              </button>
+            }
+            className="w-full"
+          />
+        </NavbarContent>
+
+        <NavbarContent justify="end">
+          <NavbarItem className="md:flex hidden">
+            <DarkModeSwitch />
+          </NavbarItem>
+          <NavbarItem className="md:flex hidden">
+            <Link to="/cart">
+              <Badge content={totalCount} color="danger" shape="circle">
+                <FaCartShopping size={24} />
+              </Badge>
+            </Link>
+          </NavbarItem>
+          <NavbarItem className="md:flex hidden">
+            <ProfileOptions />
+          </NavbarItem>
+
+          <NavbarItem
+            onClick={openNav}
+            className="flex md:hidden cursor-pointer"
+          >
+            <IoMenu size={26} />
+          </NavbarItem>
+        </NavbarContent>
+      </HeroUINav>
+
+      <MobileNav visible={showNav} onClose={closeNav} />
+    </>
   );
 };
 

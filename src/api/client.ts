20386a8e88875b5e54fb3f66d56b623713f32a26
leesinma1214 <revolver1214@gmail.com>
2025-12-05 09:53:15 +0000

    fix: update baseURL to use HTTPS for production environment

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-let baseURL = "http://ebook-platform.onrender.com";
+let baseURL = "https://ebook-platform.onrender.com";
 if (import.meta.env.MODE === "development"  ) {
   baseURL = "http://192.168.98.2:8989";
 }

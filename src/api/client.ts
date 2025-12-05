76c3e2687f9cd26156d420168130c33940d9c1cb
leesinma1214 <revolver1214@gmail.com>
2025-12-05 09:37:06 +0000

    fix: update axios client baseURL configuration for better environment handling

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,8 +1,12 @@
 import axios from "axios";
 
+let baseURL = "http://ebook-platform.onrender.com";
+if (import.meta.env.MODE === "development"  ) {
+  baseURL = "http://192.168.98.2:8989";
+}
+
 const client = axios.create({
-  baseURL: "http://192.168.98.2:8989",
-  // baseURL: "https://ebook-platform.onrender.com",
+  baseURL,
 });
 
 client.interceptors.request.use(function (config) {

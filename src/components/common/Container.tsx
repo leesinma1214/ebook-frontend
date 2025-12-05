ffb40a66dbf04dd116f63db3ba5a80f5334ca177
leesinma1214 <revolver1214@gmail.com>
2025-12-05 14:46:51 +0000

    fix: add padding to footer and adjust layout in Container component

--- src/components/common/Container.tsx
+++ src/components/common/Container.tsx
@@ -12,11 +12,17 @@ const Container: FC<Props> = ({ children }) => {
   const readingMode = location.pathname.startsWith("/read/");
 
   if (readingMode) return children;
-  
+
   return (
     <div className="min-h-screen max-w-5xl mx-auto flex flex-col">
       <Navbar />
-      <div className="flex-1 flex flex-col">{children}</div>
+      <div className="flex-1 flex flex-col pb-20">{children}</div>
+
+      <footer className="pb-10 px-4 text-center">
+        <p className="text-sm text-gray-500">
+          © {new Date().getFullYear()} DiGiRead. Tất cả các quyền được bảo lưu.
+        </p>
+      </footer>
     </div>
   );
 };
